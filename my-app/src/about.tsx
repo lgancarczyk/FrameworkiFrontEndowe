ffc81218d9e0f8525064359dd2e3dc8876addeb3
lgancarczyk <79982113+lgancarczyk@users.noreply.github.com>
2022-10-28 18:54:26 +0200

    Done Routing

---
+++ my-app/src/about.tsx
@@ -0,0 +1,10 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {FC} from 'react'
+
+export const About:FC = props =>{
+    return (
+      <div>
+        This is about
+      </div>
+    );
+};
---
+++ my-app/src/home.tsx
@@ -0,0 +1,10 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {FC} from 'react'
+
+export const Home:FC = props =>{
+    return (
+      <div>
+        This is home
+      </div>
+    );
+};
